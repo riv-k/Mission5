ba25c7fede4fa491155a91fad0dfefea9fc4fb71
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-26 10:51:23 +1300

    added try/catch to CLI tests

--- db-cli/tests/cli.test.js
+++ db-cli/tests/cli.test.js
@@ -1,7 +1,6 @@
 const { exec } = require("child_process");
 const mongoose = require("mongoose");
 const AuctionItem = require("../models/AuctionItem");
-const { error } = require("console");
 
 const MONGO_URI = "mongodb://localhost:27017/mission5db";
 
@@ -30,23 +29,27 @@ describe("CLI Tests", () => {
 
   it("should add real seed data to the database", (done) => {
     exec(`mission5 seed`, async (error, stdout, stderr) => {
-      expect(stdout).toContain("Seed data inserted\n");
-
-      const items = await AuctionItem.find({});
-      expect(items.length).toBe(3); // change if we modify seed data
-
-      done();
+      try {
+        expect(stdout).toContain("Seed data inserted\n");
+        const items = await AuctionItem.find({});
+        expect(items.length).toBe(3); // change if we modify seed data
+        done();
+      } catch (error) {
+        done(error);
+      }
     });
   });
 
   it("should clear the seed data from the database", (done) => {
     exec(`mission5 unseed`, async (error, stdout, stderr) => {
-      expect(stdout).toContain("Seed data removed\n");
-
-      const items = await AuctionItem.find({});
-      expect(items.length).toBe(0);
-
-      done();
+      try {
+        expect(stdout).toContain("Seed data removed\n");
+        const items = await AuctionItem.find({});
+        expect(items.length).toBe(0);
+        done();
+      } catch (error) {
+        done(error);
+      }
     });
   });
 
@@ -55,8 +58,8 @@ describe("CLI Tests", () => {
       `mission5 seed`,
       { env: { ...process.env, MONGO_URI: "mongodb://invalidhost:27017" } },
       (error, stdout, stderr) => {
-        console.log("STDOUT:", stdout);
-        console.log("STDERR:", stderr);
+        // console.log("STDOUT:", stdout);
+        // console.log("STDERR:", stderr);
         expect(stderr).toContain("Error seeding database:");
         done();
       }
@@ -66,10 +69,13 @@ describe("CLI Tests", () => {
   it("should not duplicate data on multiple seed runs", (done) => {
     exec(`mission5 seed`, () => {
       exec(`mission5 seed`, async () => {
-        const items = await AuctionItem.find({});
-        expect(items.length).toBe(3); // should still be 3
-
-        done();
+        try {
+          const items = await AuctionItem.find({});
+          expect(items.length).toBe(3); // should still be 3 && change if we modify seed data
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
     });
   });
@@ -77,10 +83,14 @@ describe("CLI Tests", () => {
   it("should not throw error on multiple unseed runs", (done) => {
     exec(`mission5 unseed`, () => {
       exec(`mission5 unseed`, async (error, stdout, stderr) => {
-        const items = await AuctionItem.find({});
-        expect(items.length).toBe(0); // should still be 0
-        expect(stdout).toContain("Seed data removed\n");
-        done();
+        try {
+          const items = await AuctionItem.find({});
+          expect(items.length).toBe(0); // should still be 0
+          expect(stdout).toContain("Seed data removed\n");
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
     });
   });
