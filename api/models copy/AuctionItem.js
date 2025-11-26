af59f2fddccd11dab453a8a3774896d5deb20161
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-26 14:06:44 +1300

    init setup of the API

---
+++ api/models copy/AuctionItem.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const AuctionItemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  start_price: {
+    type: Number,
+    required: true,
+  },
+  reserve_price: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("AuctionItem", AuctionItemSchema);
