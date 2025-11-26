75c64617013453d22a8c8bc20c0c96302a3d830c
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-26 14:24:14 +1300

    FIXED

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const itemsRouter = require('./routes/items');
+
+app.use('/api/items', itemsRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
